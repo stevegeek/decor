52499ffc2b74da815a13591ec66e0682a1571571
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-06-17 15:05:05 +0200

    Element and Spinner

---
+++ app/components/decor/element.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# An element is simply a generic container that can be used to wrap other components.
+# Useful to add custom Stimulus controllers/attributes around other components.
+module Decor
+  class Element < PhlexComponent
+    # TODO: use literal ** ?
+    attribute :options
+
+    def view_template
+      render parent_element do
+        yield
+      end
+    end
+
+    def root_element_attributes
+      @options
+    end
+  end
+end
