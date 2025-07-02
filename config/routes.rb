98386e3b8bf03c016b7ddd6ca85bf7d01703cb96
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-07-02 16:17:31 +0200

    App root route only set if env var set, but lookbook mounted also in dev

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  if ENV["PROD_LOOKBOOK_ENABLED"] == "true"
+  if ENV["PROD_LOOKBOOK_ENABLED"] == "true" || Rails.env.local?
     mount Lookbook::Engine, at: "/lookbook"
-    # Defines the root path route ("/")
+  end
+
+  if ENV["PROD_LOOKBOOK_ENABLED"] == "true"
     root to: redirect("/lookbook")
   end
 end
