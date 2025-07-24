be272d26d2151eb2360ed89844f77c6e8208ccd7
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-07-24 15:05:36 +0200

    Fix importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,6 @@ pin "swiper" # @11.2.10
 
 pin "controllers/decor"
 pin "controllers/decor/http"
-
+  
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "cally" # @0.8.0
-pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.11.1/es/index.js"
--- vendor/javascript/swiper.js
+++ vendor/javascript/swiper.js
@@ -1,4 +1,4 @@
-// swiper@11.2.8 downloaded from https://ga.jspm.io/npm:swiper@11.2.8/swiper.mjs
+// swiper@11.2.10 downloaded from https://ga.jspm.io/npm:swiper@11.2.10/swiper.mjs
 
-export{S as Swiper,S as default}from"./_/BXVBQGyQ.js";import"./_/BHGAM-o7.js";
+export{S as Swiper,S as default}from"./_/Dhs5nmhr.js";import"./_/BHGAM-o7.js";
 
