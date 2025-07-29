80281bfd546cf150e54c626ae9cf61776079b08e
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-07-29 18:51:16 +0200

    Fix highlight import

--- app/javascript/controllers/decor/code_block_controller.js
+++ app/javascript/controllers/decor/code_block_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { HighlightJS } from "highlight.js"
+import HighlightJS from "@highlightjs/cdn-assets/es/highlight.min.js"
 
 export default class extends Controller {
   static targets = ["code"]
