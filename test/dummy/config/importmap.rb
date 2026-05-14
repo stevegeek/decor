da70672dbb80eb15f1206d2a3c675869ed78a0da
Stephen Ierodiaconou <stevegeek@gmail.com>
2026-05-14 18:28:59 +0000

    Remove external-svg-loader (no longer needed; sprite-based icons use native <use>)

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -6,8 +6,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-# External SVG loader for dynamic SVG loading
-pin "external-svg-loader" # @1.7.1
 pin "swiper" # @11.2.10
 
 pin "controllers/decor"
