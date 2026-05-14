afe6cfa4fa0b5cd3aeac2840234735fc69b1ad8b
Stephen Ierodiaconou <stevegeek@gmail.com>
2026-05-14 12:21:38 +0000

    Add Decor preview path to Rails autoload so preview constants resolve

--- lib/decor/engine.rb
+++ lib/decor/engine.rb
@@ -13,12 +13,15 @@ module Decor
       app.config.assets.paths << root.join("app/assets/images")
     end
 
-    initializer "decor.lookbook" do |app|
+    initializer "decor.lookbook", before: :set_autoload_paths do |app|
       next unless defined?(::Lookbook)
       preview_dir = root.join("test/components/previews").to_s
       page_dir = root.join("test/components/docs").to_s
       app.config.lookbook.preview_paths = Array(app.config.lookbook.preview_paths) + [preview_dir]
       app.config.lookbook.page_paths = Array(app.config.lookbook.page_paths) + [page_dir]
+      # Register preview path with Rails autoload so Decor::Daisy::*Preview constants resolve.
+      app.config.autoload_paths += [preview_dir]
+      app.config.eager_load_paths += [preview_dir]
     end
   end
 end
